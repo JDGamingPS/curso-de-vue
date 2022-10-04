9049d07d63e00e04c9496aba2e0499921619e727
JDGamingPS <gamingrpm1@gmail.com>
2022-10-03 23:01:21 -0400

    vue interpolacion

---
+++ cursosvue/vuejs/mi-primer-vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup> 
+  const name = 'Vue dinamico'
+  const styleColor = 'color:blue';
+</script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1> <hr>
+  <h2 v-bind:style = "styleColor">  Soy azul</h2>
+</template>
+
+<style>
+  h1{
+    color: red
+  }
+</style>
